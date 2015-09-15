780e2f0019f0391f5314af63ef590d7f523e2ccc
Vipul <vipul@vipul.(none)>
2015-09-15 12:26:24 +0530

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    #root 'welcome#index'
-   get "log_out" => "sessions#destroy", :as => "log_out"
-get "log_in" => "sessions#new", :as => "log_in"
-get "sign_up" => "users#new", :as => "sign_up"
-root :to => "users#new"
-resources :users
-resources :sessions
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
+  resources :sessions
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
